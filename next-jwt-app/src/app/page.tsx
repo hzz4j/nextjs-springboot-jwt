cb534942a3527d467c97c29d74769774996f3061
pkmer <q10viking@outlook.com>
2024-12-14 00:26:22 +0800

    feat(hello): :sparkles: hello

--- next-jwt-app/src/app/page.tsx
+++ next-jwt-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <section className='container relative mx-auto flex text-red-400'>hello world</section>
+      <section className='container relative mx-auto flex text-red-400'>hello</section>
     </>
   )
 }
