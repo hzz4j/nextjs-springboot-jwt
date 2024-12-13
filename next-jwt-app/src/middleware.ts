0b038a848c25bd44505e0b232129b37b0ea327ca
pkmer <q10viking@outlook.com>
2024-12-13 22:11:25 +0800

    feat(middleware): :sparkles: 成功添加middleware

---
+++ next-jwt-app/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export default function middleware(request: NextRequest) {
+  // TODO:JWT 验证
+  console.log(request.nextUrl)
+  console.log('see me？')
+  return NextResponse.json({ name: 'hello middleware' })
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
+  ]
+}
