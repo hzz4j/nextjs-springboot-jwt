c96e6beb05836e8b6845d48bfa5f09f01233c0c3
pkmer <q10viking@outlook.com>
2024-12-15 01:27:10 +0800

    refactor(nextjs): :recycle: 重构项目结构

--- next-jwt-app/src/app/login/page.tsx
+++ next-jwt-app/src/app/login/page.tsx
@@ -2,7 +2,7 @@ import RectCheckbox from '@/ui/login/rect-checkbox'
 export default function Page() {
   return (
     <section className='flex justify-center'>
-      <RectCheckbox />
+      <RectCheckbox width={100} />
     </section>
   )
 }
--- next-jwt-app/src/ui/login/rect-checkbox.tsx
+++ next-jwt-app/src/ui/login/rect-checkbox.tsx
@@ -3,7 +3,12 @@ import { ArrowUpLeftIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import rectCheckbox from './rect-checkbox.module.scss'
 import { useState } from 'react'
-export default function RectCheckbox() {
+
+interface Props {
+  width?: number // default 60
+}
+
+const RectCheckbox: React.FC<Props> = ({ width = 60 }) => {
   const [checked, setChecked] = useState(false)
 
   const ballStyle = checked
@@ -11,7 +16,7 @@ export default function RectCheckbox() {
         transform: 'translate(-10px, -50%) rotate(0deg)'
       } satisfies React.CSSProperties)
     : ({
-        transform: `translate(calc(${rectCheckbox.width} - 20px),-50%) rotate(-270deg)`
+        transform: `translate(calc(${width}px - 20px),-50%) rotate(-270deg)`
       } satisfies React.CSSProperties)
 
   function handleChecked(e: React.ChangeEvent<HTMLInputElement>) {
@@ -21,7 +26,13 @@ export default function RectCheckbox() {
   return (
     <section className='my-1 py-2'>
       <input type='checkbox' id='rect-checkbox' onChange={handleChecked} className='hidden' />
-      <label htmlFor='rect-checkbox' className={rectCheckbox.label}>
+      <label
+        htmlFor='rect-checkbox'
+        style={{
+          width: `${width}px`
+        }}
+        className={rectCheckbox.label}
+      >
         <div style={ballStyle}>
           <ArrowUpLeftIcon
             style={{
@@ -35,3 +46,5 @@ export default function RectCheckbox() {
     </section>
   )
 }
+
+export default RectCheckbox
