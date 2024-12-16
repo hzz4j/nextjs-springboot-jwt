2da3d9d5ee09562673696e02fcb8a9768b802141
pkmer <q10viking@outlook.com>
2024-12-16 16:18:16 +0800

    feat(修复数据校验): :sparkles: 修复数据校验

--- next-jwt-app/src/ui/login/Form/form.tsx
+++ next-jwt-app/src/ui/login/Form/form.tsx
@@ -1,12 +1,20 @@
 import { LockClosedIcon, AtSymbolIcon, UserIcon } from '@heroicons/react/24/solid'
 import style from './form.module.scss'
 import { registerUser, login } from '@/lib/actions'
-import { useActionState } from 'react'
+import { useActionState, startTransition } from 'react'
 export const LoginForm: React.FC = () => {
-  const [_state, formActive] = useActionState(login, {})
+  const [state, formAction] = useActionState(login, {})
+
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    startTransition(() => {
+      formAction(new FormData(e.currentTarget))
+    })
+  }
+
   return (
     <>
-      <form action={formActive} className='space-y-4 rounded-lg bg-gray-600 p-10'>
+      <form onSubmit={handleFormSubmit} className='space-y-4 rounded-lg bg-gray-600 p-10'>
         <h1
           style={{
             color: style.mainColor
@@ -15,24 +23,46 @@ export const LoginForm: React.FC = () => {
         >
           登录
         </h1>
-        <div className='relative'>
-          <input type='text' name='email' placeholder='Your Email' className={style.Input} />
-          <AtSymbolIcon
-            style={{
-              color: style.mainColor
-            }}
-            className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
-          />
+        <div>
+          <div className='relative' aria-describedby='email-error'>
+            <input type='text' name='email' placeholder='Your Email' className={style.Input} />
+            <AtSymbolIcon
+              style={{
+                color: style.mainColor
+              }}
+              className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
+            />
+          </div>
+          <div id='email-error' aria-atomic='true' aria-live='polite'>
+            {state.errors?.email &&
+              state.errors.email.map(error => (
+                <p key={error} className='mt-1 text-sm text-red-500'>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
+        {/* email end */}
 
-        <div className='relative'>
-          <input type='password' name='password' placeholder='Your Password' className={style.Input} />
-          <LockClosedIcon
-            style={{
-              color: style.mainColor
-            }}
-            className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
-          />
+        {/* password start */}
+        <div>
+          <div className='relative' aria-describedby='password-error'>
+            <input type='password' name='password' placeholder='Your Password' className={style.Input} />
+            <LockClosedIcon
+              style={{
+                color: style.mainColor
+              }}
+              className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
+            />
+          </div>
+          <div id='password-error' aria-atomic='true' aria-live='polite'>
+            {state.errors?.password &&
+              state.errors.password.map(error => (
+                <p key={error} className='mt-1 text-sm text-red-500'>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         <div className='flex justify-center'>
@@ -40,7 +70,7 @@ export const LoginForm: React.FC = () => {
             style={{
               backgroundColor: style.mainColor
             }}
-            className='rounded-md border px-3 py-2 text-white'
+            className='rounded-md border px-5 py-1 text-base text-white'
           >
             提交
           </button>
@@ -51,8 +81,7 @@ export const LoginForm: React.FC = () => {
 }
 
 export const SignUpForm: React.FC = () => {
-  const [_state, formActive] = useActionState(registerUser, {})
-  console.log({ _state })
+  const [state, formActive] = useActionState(registerUser, {})
   return (
     <>
       <form action={formActive} className='space-y-4 rounded-lg bg-gray-100 p-10'>
@@ -64,36 +93,69 @@ export const SignUpForm: React.FC = () => {
         >
           注册
         </h1>
-
-        <div className='relative'>
-          <input type='text' name='username' placeholder='Your Full Name' className={style.Input} />
-          <UserIcon
-            style={{
-              color: style.mainColor
-            }}
-            className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
-          />
+        {/* username start */}
+        <div>
+          <div className='relative' aria-describedby='username-error'>
+            <input type='text' name='username' placeholder='Your Full Name' className={style.Input} />
+            <UserIcon
+              style={{
+                color: style.mainColor
+              }}
+              className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
+            />
+          </div>
+          <div id='username-error' aria-atomic='true' aria-live='polite'>
+            {state.errors?.username &&
+              state.errors.username.map(error => (
+                <p key={error} className='mt-1 text-sm text-red-500'>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+        {/* username end */}
+        {/* email start */}
+        <div>
+          <div className='relative'>
+            <input type='text' name='email' placeholder='Your Email' className={style.Input} />
+            <AtSymbolIcon
+              style={{
+                color: style.mainColor
+              }}
+              className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
+            />
+          </div>
+          <div id='email-error' aria-atomic='true' aria-live='polite'>
+            {state.errors?.email &&
+              state.errors.email.map(error => (
+                <p key={error} className='mt-1 text-sm text-red-500'>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+        {/* email end */}
+        {/* password start */}
+        <div>
+          <div className='relative' aria-describedby='password-error'>
+            <input type='password' name='password' placeholder='Your Password' className={style.Input} />
+            <LockClosedIcon
+              style={{
+                color: style.mainColor
+              }}
+              className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
+            />
+          </div>
+          <div id='password-error' aria-atomic='true' aria-live='polite'>
+            {state.errors?.password &&
+              state.errors.password.map(error => (
+                <p key={error} className='mt-1 text-sm text-red-500'>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
-        <div className='relative'>
-          <input type='text' name='email' placeholder='Your Email' className={style.Input} />
-          <AtSymbolIcon
-            style={{
-              color: style.mainColor
-            }}
-            className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
-          />
-        </div>
-
-        <div className='relative'>
-          <input type='password' name='password' placeholder='Your Password' className={style.Input} />
-          <LockClosedIcon
-            style={{
-              color: style.mainColor
-            }}
-            className='absolute left-1 top-1/2 h-5 w-5 -translate-y-1/2'
-          />
-        </div>
         <div className='flex justify-center'>
           <button
             style={{
