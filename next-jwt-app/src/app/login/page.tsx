c023eb1ca73602b2a5d15e50728a5a03455b40de
pkmer <q10viking@outlook.com>
2024-12-16 09:34:21 +0800

    feat(login): :sparkles: 完成切换动画

--- next-jwt-app/src/app/login/page.tsx
+++ next-jwt-app/src/app/login/page.tsx
@@ -1,8 +1,45 @@
+'use client'
+import { useState } from 'react'
+import clsx from 'clsx'
 import RectCheckbox from '@/ui/login/rect-checkbox'
+import styleVariable from './page.module.scss'
+
 export default function Page() {
+  const [isChecked, setChecked] = useState(true)
+  const isLogin = isChecked
+  const isSignUp = !isChecked
+
+  function handleOnChange(checked: boolean) {
+    console.log(checked)
+    setChecked(checked)
+  }
+
+  const liStyle = {
+    transition: `color ${styleVariable.duration} ease-in-out`
+  } satisfies React.CSSProperties
+
   return (
-    <section className='flex justify-center'>
-      <RectCheckbox width={100} />
+    <section className='flex flex-col items-center justify-center gap-6'>
+      <ul className='flex gap-[80px]'>
+        <li
+          style={liStyle}
+          className={clsx('text-xl font-bold text-gray-300', {
+            '!text-black': isLogin
+          })}
+        >
+          登录
+        </li>
+
+        <li
+          style={liStyle}
+          className={clsx('text-xl font-bold text-gray-300', {
+            '!text-black': isSignUp
+          })}
+        >
+          注册
+        </li>
+      </ul>
+      <RectCheckbox width={60} onChange={handleOnChange} />
     </section>
   )
 }
